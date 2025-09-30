a43d85a440ae3bb44eeffa59b1b235baba9d9345
Deepak Malpani <deepak.malpani@exxonmobil.com>
2025-09-30 22:02:22 +0530

    Refactor v1

--- monitor.py
+++ monitor.py
@@ -2,36 +2,25 @@
 from time import sleep
 import sys
 
+def print_loading_statement():
+  for i in range(6):
+      print('\r* ', end='')
+      sys.stdout.flush()
+      sleep(1)
+      print('\r *', end='')
+      sys.stdout.flush()
+      sleep(1)
 
 def vitals_ok(temperature, pulseRate, spo2):
   if temperature > 102 or temperature < 95:
-    print('Temperature critical!')
-    for i in range(6):
-      print('\r* ', end='')
-      sys.stdout.flush()
-      sleep(1)
-      print('\r *', end='')
-      sys.stdout.flush()
-      sleep(1)
+    print_loading_statement()
     return False
   elif pulseRate < 60 or pulseRate > 100:
     print('Pulse Rate is out of range!')
-    for i in range(6):
-      print('\r* ', end='')
-      sys.stdout.flush()
-      sleep(1)
-      print('\r *', end='')
-      sys.stdout.flush()
-      sleep(1)
+    print_loading_statement()
     return False
   elif spo2 < 90:
     print('Oxygen Saturation out of range!')
-    for i in range(6):
-      print('\r* ', end='')
-      sys.stdout.flush()
-      sleep(1)
-      print('\r *', end='')
-      sys.stdout.flush()
-      sleep(1)
+    print_loading_statement()
     return False
   return True
