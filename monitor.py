eb4e7711bf25c828d08a778eef30f67ca7b086b2
Deepak Malpani <deepak.malpani@exxonmobil.com>
2025-09-30 22:15:48 +0530

    Refactor v3

--- monitor.py
+++ monitor.py
@@ -6,17 +6,26 @@ def show_loading(number_of_seconds):
       print('\r* ', end='')
       sys.stdout.flush()
       sleep(1)
-  
-def vitals_ok(temperature, pulseRate, spo2):
+
+def check_temperature(temperature):
   if temperature > 102 or temperature < 95:
     show_loading(12)
     return False
-  if pulseRate < 60 or pulseRate > 100:
+  return True
+
+def check_pulserate(pulse_rate):
+  if pulse_rate < 60 or pulse_rate > 100:
     print('Pulse Rate is out of range!')
     show_loading(12)
     return False
+  return True
+
+def check_spo2(spo2):
   if spo2 < 90:
     print('Oxygen Saturation out of range!')
     show_loading(12)
     return False
   return True
+  
+def vitals_ok(temperature, pulse_rate, spo2):
+  return check_temperature(temperature) and check_pulserate(pulse_rate) and check_spo2(spo2)
