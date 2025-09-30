77be64372f2991bc304949acdf8c8c1ebebdc71b
Deepak Malpani <deepak.malpani@exxonmobil.com>
2025-09-30 22:29:28 +0530

    Refactor v5

--- check_vitals.py
+++ check_vitals.py
@@ -1,17 +1,10 @@
 from loading_utils import show_loading
 
 def check_temperature(temperature):
-  if temperature > 102 or temperature < 95:
-    show_loading(12)
-    return False
-  return True
+  return temperature > 102 or temperature < 95
 
 def check_pulserate(pulse_rate):
-  if pulse_rate < 60 or pulse_rate > 100:
-    print('Pulse Rate is out of range!')
-    show_loading(12)
-    return False
-  return True
+  return pulse_rate < 60 or pulse_rate > 100
 
 def check_spo2(spo2):
   if spo2 < 90:
@@ -21,7 +14,11 @@ def check_spo2(spo2):
   return True
   
 def vitals_ok(temperature, pulse_rate, spo2):
-  if check_temperature(temperature) : 
+  if check_temperature(temperature): 
     show_loading(12)
     return False
-  return check_pulserate(pulse_rate) and check_spo2(spo2)
+  if check_pulserate(pulse_rate):
+    print('Pulse Rate is out of range!')
+    show_loading(12)
+    return False
+  return check_spo2(spo2)
